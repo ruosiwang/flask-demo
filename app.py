295babfaf249c031eb057d4b6e15b50f1b48640f
Ruosi Wang <Ruosi@Ruosis-MacBook-Pro.local>
2018-09-09 21:24:20 -0400

    flask demo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import requests
 import pandas as pd
 import numpy as np
@@ -13,7 +13,7 @@ def datetime(x):
     return np.array(x, dtype=np.datetime64)
 
 
-stock_names = ['GOOG', 'AAPL', 'IBM', 'MSFT']
+stock_names = ['Close', 'Open', 'Adj. Close', 'Adj. Open']
 
 # use quandl api to get the data of a stock, and then return the pandaframe
 
@@ -37,66 +37,54 @@ def get_csv_data(stock):
 
 
 # create a bokeh figure
-def create_figure(stock):
+def create_figure(stock, lookup):
 
-    stock_data = get_csv_data(stock)
-    # stock_data['tp'] = stock_data.index
-    lookup = 'Adj. Close'
+    # df = get_csv_data(stock)
+    df = get_quandl_data(stock)
+    df['Date'] = pd.to_datetime(df['Date'])
+    # lookup = 'Close'
 
-    # lib = {'Close': 'Closing Prices', 'Open': 'Open Prices',
-    #       'Adj. Close': 'Adj. Closing Prices', 'Adj. Open': 'Adj. Open Prices'}
+    lib = {'Close': 'Closing Prices', 'Open': 'Open Prices',
+           'Adj. Close': 'Adj. Closing Prices', 'Adj. Open': 'Adj. Open Prices'}
 
     # figure 1: raw data
-    # p1 = figure(x_axis_type="datetime", title='Stock {}'.format(lib[lookup]))
-    p1 = figure(title='Stock Close Prices')
-    p1.grid.grid_line_alpha = 0.3
+    p1 = figure(x_axis_type="datetime", title='Stock {}'.format(lib[lookup]))
+    # p1 = figure(title='Stock Close Prices')
+    # p1.grid.grid_line_alpha = 0.6
     p1.xaxis.axis_label = 'Date'
     p1.yaxis.axis_label = 'Price'
 
-    # p1.line(datetime(stock_data['Date']), stock_data[lookup], color='#A6CEE3', legend=stock)
-    p1.line(stock_data['tp'], stock_data[lookup], color='#A6CEE3', legend=stock)
+    p1.line(df['Date'].values, df[lookup].values, color='#A6CEE3', legend=stock)
+    # p1.line(stock_data['tp'], stock_data[lookup], color='#A6CEE3', legend=stock)
     p1.legend.location = "top_left"
     return p1
 
-    # # figure 2: month average
-    # stock_raw = np.array(stock_data[lookup])
-    # stock_dates = np.array(stock_data['Date'], dtype=np.datetime64)
 
-    # window_size = 30
-    # window = np.ones(window_size) / float(window_size)
-    # stock_avg = np.convolve(stock_raw, window, 'same')
-
-    # p2 = figure(x_axis_type="datetime", title='{} One-Month Average'.format(stock))
-    # p2.grid.grid_line_alpha = 0
-    # p2.xaxis.axis_label = 'Date'
-    # p2.yaxis.axis_label = 'Price'
-    # p2.ygrid.band_fill_color = "olive"
-    # p2.ygrid.band_fill_alpha = 0.1
-
-    # p2.circle(stock_dates, stock_raw, size=4, legend='close',
-    #           color='darkgrey', alpha=0.2)
-
-    # p2.line(stock_dates, stock_avg, legend='avg', color='navy')
-    # p2.legend.location = "top_left"
-
-    # p = gridplot([[p1, p2]], plot_width=400, plot_height=400)  # open a browser
+app.vars = {}
 
 
-# Index page
 @app.route('/')
+def main():
+    return redirect('/index')
+
+
+@app.route('/index', methods=['GET'])
 def index():
-    # Determine the selected feature
-    current_stock_name = request.args.get("stock_name")
-    if current_stock_name is None:
-        current_stock_name = "AAPL"
+    return render_template('index.html', stock_names=stock_names)
+
+
+@app.route('/graph', methods=['POST'])
+def graph():
+
+    app.vars['ticker'] = request.form['ticker']
+    app.vars['lookup'] = request.form['lookup']
 
     # Create the plot
-    plot = create_figure(current_stock_name)
+    p = create_figure(app.vars['ticker'], app.vars['lookup'])
 
     # Embed plot into HTML via Flask Render
-    script, div = components(plot)
-    return render_template("stock_index.html", script=script, div=div,
-                           stock_names=stock_names, current_stock_name=current_stock_name)
+    script, div = components(p)
+    return render_template("graph.html", script=script, div=div)
 
 
 # With debug=True, Flask server will auto-reload
